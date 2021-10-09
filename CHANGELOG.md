aa3021846abcd1de7869b524f745c141a33b11d8
Soufiene-Hmila <soufiene.hmila@gmail.com>
2021-10-09 12:25:31 +0100

    fix bug ios

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,5 @@
 ## [0.0.1+1] add github repository.
 
 ## [0.0.2] fix bug ios.
+
+## [0.0.3] fix bug ios.
--- android/src/main/kotlin/com/example/path_download/PathDownloadPlugin.kt
+++ android/src/main/kotlin/com/example/path_download/PathDownloadPlugin.kt
@@ -3,7 +3,6 @@ package com.example.path_download
 import android.content.Context
 import android.os.Environment.*
 import android.util.Log
-import android.widget.Switch
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -11,6 +10,14 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import java.io.File
+import android.provider.MediaStore
+import android.R.attr.mimeType
+import android.content.ContentValues
+import android.content.ContentResolver
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
+
 
 /** PathDownloadPlugin */
 class PathDownloadPlugin: FlutterPlugin, MethodCallHandler {
@@ -24,11 +31,13 @@ class PathDownloadPlugin: FlutterPlugin, MethodCallHandler {
       applicationContext = flutterPluginBinding.applicationContext
   }
 
+  @RequiresApi(Build.VERSION_CODES.Q)
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
 
     if (call.method == "getDownloadsDirectory"){
       val typeFile : String? = call.argument("typeFile")
       result.success(getDownloadsDirectory(typeFile))
+
     }
     else result.notImplemented()
 
@@ -38,10 +47,18 @@ class PathDownloadPlugin: FlutterPlugin, MethodCallHandler {
     methodChannel.setMethodCallHandler(null)
   }
 
+  @RequiresApi(Build.VERSION_CODES.Q)
   private fun getDownloadsDirectory(typeFile : String?): String? {
 
     val fileDirectory: File? = applicationContext.getExternalFilesDir(typeFile)
     fileDirectory?.let { Log.i("$typeFile", it.absolutePath) }
-    return fileDirectory?.absolutePath
+    // return fileDirectory?.absolutePath
+
+    val resolver: ContentResolver = applicationContext.contentResolver
+    val contentValues = ContentValues()
+    contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Docum")
+    contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, DIRECTORY_DOWNLOADS)
+    val uri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+   return resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)?.path
   }
 }
